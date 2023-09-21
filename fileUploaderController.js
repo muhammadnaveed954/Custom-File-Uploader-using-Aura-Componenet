596b4190263b3b50bc2a3e3277ac4d4f596928fd
Muhammad Naveed nawab <127825866+muhammadnaveed954@users.noreply.github.com>
2023-09-21 14:30:08 +0500

    Update fileUploaderController.js

--- fileUploaderController.js
+++ fileUploaderController.js
@@ -10,11 +10,11 @@
     // TO Remove File Uploaded file on click via UI before sending it to the backend by clicking on the cross icon.
     // I used two different methods to remove the file, from UI and from the list to send it to the Record via Apex, because of Objecct difficulty. 
     
-    removeFileHandler: function (component, event, helper) {
+ removeFileHandler: function (component, event, helper) {
         let fileToRemove = event.currentTarget.getAttribute("data-index");
         let uploadedFiles = component.get("v.uploadedFiles");
 
-        // Remove file from the list to upload it to the record Via Backend
+        // Remove file from the list to upload it to the backend
 
         let updatedFile = {};
         for (let key in uploadedFiles) {
@@ -29,19 +29,29 @@
 
         let uploadedFileNames = component.get("v.uploadedFileNames");
 
-        let updatedFileNames = Object.entries(uploadedFileNames);
-        for(let i = 0; i < updatedFileNames.length; i++){
-            let arr = updatedFileNames[i];
-            arr.splice(0,1);
-            updatedFileNames[i] = arr;
-        }
-        updatedFileNames.splice(parseInt(fileToRemove),1);
-        if(updatedFileNames.length == 1){
-            component.set("v.uploadedFileNames", updatedFileNames[0]);
-        } else {
-            component.set("v.uploadedFileNames", updatedFileNames);
-        }
-
+        if (typeof uploadedFileNames === 'object' && !Array.isArray(uploadedFileNames)) {
+            let updatedFileNames = Object.entries(uploadedFileNames);
+            for(let i = 0; i < updatedFileNames.length; i++){
+                var arr = updatedFileNames[i];
+                arr.splice(0,1);
+                updatedFileNames[i] = arr;
+            }
+            updatedFileNames.splice(parseInt(fileToRemove),1);
+            if(updatedFileNames.length == 1){
+                component.set("v.uploadedFileNames", updatedFileNames[0]);
+            } else {
+                component.set("v.uploadedFileNames", updatedFileNames);
+            }
+          }
+          else{
+            console.log("updatedFileNames1", uploadedFileNames)
+            uploadedFileNames.splice(parseInt(fileToRemove),1);
+            if(uploadedFileNames.length == 1){
+                component.set("v.uploadedFileNames", uploadedFileNames[0]);
+            } else {
+                component.set("v.uploadedFileNames", uploadedFileNames);
+            }
+          }
     },
 
  // Convert the file to the string using base64. You can upload the One or more files at Once
